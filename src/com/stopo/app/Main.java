3c5c533cc5d092435d0a831a5c9937ca5185fe9a
Emanoel Murilo <107274726+Emanoel-Murilo@users.noreply.github.com>
2026-06-10 16:50:47 -0300

    fix: no module for intelij config

--- src/com/stopo/app/Main.java
+++ src/com/stopo/app/Main.java
@@ -3,7 +3,7 @@ import javax.swing.SwingUtilities;
 import com.stopo.product.view.Frame.MainFrame;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
             MainFrame frame = new MainFrame();
             frame.setVisible(true);
